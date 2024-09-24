fe39fe6884df3bb446fb8b854b399c82e5cf9304
Dylan Tin <dylant711@gmail.com>
2024-09-24 17:59:45 -0400

    still testing

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -5,10 +5,10 @@ import PlantCard from "./PlantCard";
 
 function PlantList({plants, updatePlant}) {
 
-  const plantComponents = plants.map(pet => {
-      return <Plant key={plant.id} plant={plant} updatePlant={updatePlant}/>
+  const plantComponents = plants.map(plant => {
+      return <PlantCard key={plant.id} plant={plant} updatePlant={updatePlant}/>
   })
-
+console.log(PlantCard)
   return (
     <ul className="cards">{{plantComponents}}</ul>
   );
